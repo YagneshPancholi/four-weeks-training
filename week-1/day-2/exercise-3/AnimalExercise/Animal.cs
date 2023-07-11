d3ec2a4627e1d00c07cb06622d03beb6a4d87be5
YagneshPancholi <pancholiyagnesh2001@gmail.com>
2023-07-11 17:24:48 +0530

    problem 3 solved

---
+++ week-1/day-2/exercise-3/AnimalExercise/Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using AnimalExercise;
+
+public abstract class Animal
+{
+	public string? name;
+	public int age;
+	public abstract void makeSound();
+}
+public class Dog : Animal, Imovable
+{
+	public Dog(string name, int age)
+	{
+	this.name = name;
+	this.age = age;
+	}
+	public override void makeSound()
+	{
+		Console.WriteLine("Woof Woof!!!!");
+	}
+	public  void move()
+	{
+		Console.WriteLine("Dog is moving!!!");
+	}
+}
+public class Cat : Animal, Imovable
+{
+	public Cat(string name, int age)
+	{
+		this.name = name;
+		this.age = age;
+	}
+	public override void makeSound()
+	{
+		Console.WriteLine("Meow Meow!!!!");
+	}
+	public void move()
+	{
+		Console.WriteLine("Cat is moving!!!");
+	}
+}
+interface Imovable
+{
+	void move();
+}
--- week-1/day-2/exercise-3/AnimalExercise/Program.cs
+++ week-1/day-2/exercise-3/AnimalExercise/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace AnimalExercise
+﻿using AnimalExercise;
+namespace AnimalExercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // Create a list of Animal objects, add Dog and Cat instances, and call their methods
+            List<Animal> animals = new List<Animal>()
+            {
+                new Dog("Dog1",5),
+                new Cat("Cat1",4)
+            };
+            Console.WriteLine(animals.ElementAt(0).name);
+            Console.WriteLine(animals.ElementAt(0).age);
+            animals.ElementAt(0).makeSound();
+            ((Dog)animals.ElementAt(0)).move();
+            Console.WriteLine(animals.ElementAt(1).name);
+            Console.WriteLine(animals.ElementAt(1).age);
+            animals.ElementAt(1).makeSound();
+            ((Cat)animals.ElementAt(1)).move();
+
+            //Console.WriteLine(animals.GetType());
+            Console.ReadLine();
+           
         }
     }
 }
