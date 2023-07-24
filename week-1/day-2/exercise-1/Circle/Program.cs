c4d8b9865e5bd93ad37eae2786f85ae6ef0b0eb4
YagneshPancholi <pancholiyagnesh2001@gmail.com>
2023-07-24 23:22:42 +0530

    running from vs code

--- week-1/day-2/exercise-1/Circle/Program.cs
+++ week-1/day-2/exercise-1/Circle/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             // Create a Circle object and display its area and circumference
-            Circle c1 = new Circle(1.0);
-            Console.WriteLine("Ares : "+ c1.getArea());
-            Console.WriteLine("Circumference : "+c1.getCircumference());
+            Circle c1 = new(1.0);
+            Console.WriteLine("Ares : "+ c1.GetArea());
+            Console.WriteLine("Circumference : " + c1.GetCircumference());
             Console.ReadLine();
         }
     }
